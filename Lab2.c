07f182ba64bd04c752feb5937352a2d9f7691d71
Harshithakulal238 <harshitha.23cs046@sode-edu.in>
2025-02-27 10:59:32 +0530

    Merge sort

---
+++ Lab2.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<time.h>
+
+void merge(int a[],int low,int mid,int high){
+int i=low,j=mid+1,k=low;
+int c[1000000];
+while(i<=mid && j<=high){
+if(a[i]<a[j]){
+c[k]=a[i];
+i++;
+}
+else{
+c[k]=a[j];
+j++;
+}
+k++;
+}
+while(i<=mid){
+c[k++]=a[i++];
+}
+while(j<=high){
+c[k++]=a[j++];
+}
+for(i=low;i<=high;i++){
+a[i]=c[i];
+}
+}
+
+void mergesort(int a[],int low,int high){
+if(low<high){
+int mid=low+(high-low)/2;
+mergesort(a,low,mid);
+mergesort(a,mid+1,high);
+merge(a,low,mid,high);
+}
+}
+
+void main(){
+int i,n;
+printf("Enter the size of the array:");
+scanf("%d",&n);
+int a[n];
+srand(time(NULL));
+for(i=0;i<n;i++){
+a[i]=rand()%1000000;}
+clock_t start=clock();
+mergesort(a,0,n-1);
+clock_t end=clock();
+double timetaken=((double)(end-start))/CLOCKS_PER_SEC;
+printf("The time taken to sort the array is: %f",timetaken);
+/*printf("The Sorted array is:");
+for(i=0;i<n;i++)
+printf("%d ",a[i]);*/
+}
